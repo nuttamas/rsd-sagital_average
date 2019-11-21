10323e2a01080d7588c9c71f2a2a8782faa65fc4
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-21 15:46:46 +0000

    Adds dependencies for this library

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="sagital_average",
     version="0.1.0",
     packages=find_packages(exclude=['*test']),
+    install_requires=['numpy'],
     entry_points={
         'console_scripts': [
             'sagverage = sagital_average.command:process'
