dd676a363c7189aa770f038f08edc4cffd669f1a
David Perez-Suarez <d.perez-suarez@ucl.ac.uk>
2019-11-21 15:45:04 +0000

    Moves the executing script as a shell command accessible from anywhere

--- setup.py
+++ setup.py
@@ -4,4 +4,8 @@ setup(
     name="sagital_average",
     version="0.1.0",
     packages=find_packages(exclude=['*test']),
+    entry_points={
+        'console_scripts': [
+            'sagverage = sagital_average.command:process'
+            ]},
 )
