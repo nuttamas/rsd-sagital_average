a611cb2393876f94cf6f5758e0abc7ee84d4ae2b
NuttamasT <Nuttamas.tubsrinuan.20@ucl.ac.uk>
2020-12-23 15:15:48 +0000

    Edit setup file and remve the __main__ block in fn

--- setup.py
+++ setup.py
@@ -5,4 +5,5 @@ setup(
     name="sagital_average",
     version="0.0.1",
     packages=find_packages(exclude=['*test']),
+    entry_points={ 'console_scripts': ['sagverage = sagital_average.command:process']},
 )
