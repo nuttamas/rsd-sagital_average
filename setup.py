6028f658cfe0bba4f7c45a80b2adbc6d5cb0418e
NuttamasT <Nuttamas.tubsrinuan.20@ucl.ac.uk>
2020-12-23 15:17:16 +0000

    Add dependency that this package needs numpy

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ setup(
     name="sagital_average",
     version="0.0.1",
     packages=find_packages(exclude=['*test']),
+    install_requires=['numpy'],  # The program needs numpy package
     entry_points={ 'console_scripts': ['sagverage = sagital_average.command:process']},
 )
