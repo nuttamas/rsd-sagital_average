d2bbdbe1b9d683ceff05dab3ac6a952bbdcb07c5
NuttamasT <Nuttamas.tubsrinuan.20@ucl.ac.uk>
2020-12-23 15:02:57 +0000

    Edit test file to read&write results in test_dir

--- tests/test_sagital_brain.py
+++ tests/test_sagital_brain.py
@@ -1,19 +1,23 @@
 import numpy as np
-import subprocess
+from pathlib import Path
+from .sagital_brain import run_averages
+
+Test_Directory = Path(__file__).parent  # Tell the program where the test directory is
 
 def test_average():
     # Creates input file
     data_input = np.zeros((20, 20))
     data_input[-1, :] = 1
+
     # The expeted result is all zeros, except the last one, it should be 1
     expected = np.zeros(20)
     expected[-1] = 1
 
-    np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+    np.savetxt(Test_Directory/"brain_sample.csv", data_input, fmt='%d', delimiter=',')
 
     # run python program
-    subprocess.run(["python", "sagital_brain.py"])
+    run_averages(file_input=Test_Directory/ "brain_sample.csv", file_output=Test_Directory/ "brain_average.csv")
 
     # Check result
-    result = np.loadtxt("brain_average.csv",  delimiter=',')
+    result = np.loadtxt(Test_Directory/"brain_average.csv",  delimiter=',')
     np.testing.assert_array_equal(result, expected)
